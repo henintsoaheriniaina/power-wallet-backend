a96073f07209ded19e14b139acb32473cc6d7359
henintsoaheriniaina <belouhtsoa@gmail.com>
2025-04-25 10:47:19 +0300

    add error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import e from "express";
+import errorHandler from "./middlewares/error";
 import logMiddleware from "./middlewares/log";
 const app = e();
 
 app.use(cors());
 app.use(e.json());
 app.use(cookieParser());
+
+// custom middlewares
 app.use(logMiddleware);
+app.use(errorHandler);
 export default app;
